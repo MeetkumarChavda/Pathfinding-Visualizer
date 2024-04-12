373aea82af6a8fea78aedabf99333ed125b9e24d
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-04-12 09:20:12 +0530

    Board Interaction + guide-bar added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var cells;
+let row, col;
+var matrix = [];
 const board  = document.querySelector("#board");
 
 renderBoard();
@@ -6,16 +8,20 @@ renderBoard();
 function renderBoard(cellWidth = 22){
 
     cells = [];
-    let row = Math.floor(board.clientHeight / cellWidth) ;
-    let col = Math.floor(board.clientWidth / cellWidth) ;
+    row = Math.floor(board.clientHeight / cellWidth) ;
+    col = Math.floor(board.clientWidth / cellWidth) ;
+    
+    // Update CSS variable for cell width
     const root = document.documentElement;
     root.style.setProperty('--cell-width', `${cellWidth}`);
     // console.log(row , col);
 
     // To prevent appending previous board
     board.innerHTML ='';
-   
+    
+     // Generate grid structure
     for (let r = 0 ; r < row ; r++ ) {
+        const rowArr = [];
         const rowElement = document.createElement('div');
         rowElement.classList.add("row");
         rowElement.setAttribute('id',`${r}`); 
@@ -23,37 +29,45 @@ function renderBoard(cellWidth = 22){
         for (let c = 0; c<col; c++) {   
             const colElement = document.createElement('div');
             colElement.classList.add("col");
-            colElement.setAttribute('id',`${c}`);
+            //forgot to add row address id so added now 
+            colElement.setAttribute('id',`${r}-${c}`);
 
             cells.push(colElement);
+            rowArr.push(colElement); 
             rowElement.appendChild(colElement);
         }
+        matrix.push(rowArr);
         board.appendChild(rowElement);
     }
 }
-
-
-const navOptions = document.querySelectorAll('.nav-menu>li>a');
+// console.log(matrix);
 var dropOptions = null ;
+const navOptions = document.querySelectorAll('.nav-menu>li>a');
+
+// Event listener for each navigation option
 navOptions.forEach(navOption =>{
     navOption.addEventListener('click' ,()=>{
         const li = navOption.parentElement;
-        // self toggle
+         // Self toggle: if already active, deactivate
         if(li.classList.contains('active')){
             li.classList.remove('active');
             return;
         }
-
+        // Deactivate all active elements before activating the clicked one
         removeActive(navOptions , true);
         li.classList.add('active');
-
+         // If the list item has a dropdown, handle the dropdown
         if(li.classList.contains('drop-box')){
             dropOptions = li.querySelectorAll('.drop-menu>li');
             toggle_dropOption(navOption.innerText);
         }
     })
-})
-
+});
+/**
+ * Removes the 'active' class from a collection of elements or their parent elements.
+ * @param {NodeListOf<Element>} elements - The elements from which to remove the 'active' class.
+ * @param {boolean} parent - If true, the 'active' class will be removed from the parent elements.
+ */
 const removeActive = (elements , parent = false )=>{
     elements.forEach(element =>{
         if(parent) element = element.parentElement;
@@ -66,6 +80,10 @@ let speed = 'normal';
 let algorithm = 'BFS' ;
 const visualizeBtn = document.getElementById("visualize");
 
+/**
+ * Handles click events on dropdown options and updates the application state based on the selection.
+ * @param {string} target - The category of the dropdown ('pixel', 'speed', 'algorithms').
+ */
 function toggle_dropOption(target){
     // console.log(target)
     dropOptions.forEach(dropOption=>{
@@ -86,7 +104,7 @@ function toggle_dropOption(target){
                 visualizeBtn.innerText = `Visualize ${algorithm}`
                 // console.log(algorithm);
             }
-            // remove dropdown after selecting item from drop-d-menu
+             // Deactivate dropdown menu after selection
             removeActive(navOptions , true);
         })
     })
@@ -97,4 +115,110 @@ document.addEventListener('click' , (e)=>{
         removeActive(navOptions , true);
     }
     // console.log(e.target);
-})
+});
+
+// board interaction
+
+/**
+ * Checks if the given coordinates (x, y) are within the bounds of a specified grid size.
+ *
+ * @param {number} x - The x-coordinate to check.
+ * @param {number} y - The y-coordinate to check.
+ * @returns {boolean} True if both coordinates are within the grid bounds, false otherwise.
+ */
+function isValid(x , y){
+    const isXValid = x >= 0 && x < row;
+    const isYValid = y >= 0 && y < col;
+
+    return isXValid && isYValid;
+}
+
+
+let source_Cordinate = set('source');
+let target_Cordinate = set('target');
+/**
+ * Sets a class to an element in a matrix at provided coordinates,
+ * or at random coordinates if provided ones are invalid.
+ *
+ * @param {string} className - The CSS class name to add.
+ * @param {number} [x=-1] - Optional x-coordinate, defaults to -1 to indicate random placement.
+ * @param {number} [y=-1] - Optional y-coordinate, defaults to -1 to indicate random placement.
+ * @returns {Object} The coordinates {x, y} where the class was added.
+ */
+function set(className , x= -1 , y = -1){
+    if(isValid(x,y)){
+        matrix[x][y].classList.add(className);
+    }else{
+        x = Math.floor(Math.random() * row);
+        y = Math.floor(Math.random() * col);
+        matrix[x][y].classList.add(className);
+    }
+    return {x,y};
+}
+
+let isDrawing = false;
+let isDragging = false;
+let DragPoint = null;
+
+cells.forEach((cell) => {
+    const pointerdown = (e)=> {
+        if(e.target.classList.contains('source')) {
+            DragPoint = 'source'
+            isDragging = true;
+        }else if(e.target.classList.contains('target')){
+            DragPoint = 'target'
+            isDragging = true;
+        }
+        else{
+            isDrawing = true;
+        }
+
+    }
+    const pointermove = (e)=>{
+
+        if(isDrawing){
+            e.target.classList.add('wall');
+        }else if(DragPoint && isDragging){
+            // Sequance matters
+            // 1
+            cells.forEach(cell=>{
+                cell.classList.remove(`${DragPoint}`);
+            });
+            // 2
+            e.target.classList.add(`${DragPoint}`);
+            cordinate = e.target.id.split('-');
+            if(DragPoint ==='source'){
+                source_Cordinate.x = +cordinate[0];
+                source_Cordinate.y = +cordinate[1];
+            }else{
+                target_Cordinate.x = +cordinate[0];
+                target_Cordinate.y = +cordinate[1];
+            }
+            // console.log(source_Cordinate);
+            // console.log(target_Cordinate);
+        }
+    }
+    const pointerup = ()=>{
+        isDrawing = false;
+        isDragging = false;
+        DragPoint = null;
+    }
+    cell.addEventListener('pointerdown', pointerdown);
+    cell.addEventListener('pointermove', pointermove);
+    cell.addEventListener('pointerup', pointerup);
+    cell.addEventListener('click',()=>{
+        cell.classList.toggle('wall');
+    })
+});
+
+const clearPath = ()=> {
+    cells.forEach(cell=>{
+        cell.classList.remove('path');
+    })
+}
+
+const clearWall = ()=> {
+    cells.forEach(cell=>{
+        cell.classList.remove('wall');
+    })
+}
